a26e03ba02cb1a2dee630233a0abe1cbf1c2c743
liamjbennett <liam.j.bennett@gmail.com>
2014-01-13 18:03:45 +0000

    updating tests to run against windows 2012

--- spec/system/operatingsystemversion_spec.rb
+++ spec/system/operatingsystemversion_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper_system'
 
 describe 'operatingsystemversion fact should return the full name of the operating system, its version and edition' do
   it 'operatingsystemversion fact should return full name' do
-    expected_os = 'Windows Server 2008 R2 Standard'
+    
+    case ENV['RS_SET']
+    when 'windows_2008r2'
+      expected_os = 'Windows Server 2008 R2 Standard'
+    when 'windows_2012'
+      expected_os = 'Windows Server 2012 Standard Evaluation'
+    else
+      raise("do not support #{ENV['RS_SET']}")
+    end
     
 		facter(:puppet => true) do |r|
 		  r.stderr.should be_empty
--- spec/system/windows_sid_spec.rb
+++ spec/system/windows_sid_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper_system'
 
 describe 'windows_sid fact should return the unique system sid' do
   it 'windows_sid fact should a correctly formatted sid id' do
-    expected_sid = 'S-1-5-21-3727808034-4027355127-1325899715-1000'
+    
+    case ENV['RS_SET']
+    when 'windows_2008r2'
+      expected_sid = 'S-1-5-21-3727808034-4027355127-1325899715-1000'
+    when 'windows_2012'
+      expected_sid = 'S-1-5-21-852130217-1139538421-4080395968'
+    else
+      raise("do not support #{ENV['RS_SET']}")
+    end
     
 		facter(:puppet => true) do |r|
 		  r.stderr.should be_empty
