da285573b1ccc9b05a72a103a26599b1b50205ea
liamjbennett <lbennett@opentable.com>
2014-10-08 10:32:12 +0100

    bring modules into some standard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,1 @@
-dir = File.expand_path(File.dirname(__FILE__))
-$LOAD_PATH.unshift File.join(dir, 'lib')
-
-require 'pathname'
-require 'tmpdir'
-
-Pathname.glob("#{dir}/shared_behaviours/**/*.rb") do |behaviour|
-  require behaviour.relative_path_from(Pathname.new(dir))
-end
-
-def fixture_path
-	Pathname.new(File.expand_path(File.join(__FILE__, '..', 'fixtures')))
-end
-
-RSpec.configure do |config|
-	config.mock_with :mocha
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
